88a6a055f327cb7c0c643f3d87f54cba0d0523a5
Guy Wilson <wilson.guy@gmail.com>
2021-11-28 17:50:47 +0000

    Handle C++

--- src/strutils.h
+++ src/strutils.h
@@ -1,9 +1,17 @@
 #ifndef _INCL_STRUTILS
 #define _INCL_STRUTILS
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char * str_trim_trailing(const char * str);
 char * str_trim_leading(const char * str);
 char * str_trim(const char * str);
 int str_endswith(char * src, const char * suffix);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
